b7a837c6e1dfade2ff0651bd926d3a9885614b91
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-02-13 16:32:35 -0500

    Initial Commit

---
+++ Lab1_Jorrel_Tigbayan/Lab1_Jorrel_TigbayanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Jorrel_TigbayanApp.swift
+//  Lab1_Jorrel_Tigbayan
+//
+//  Created by Jorrel Tigbayan on 2025-02-13.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Jorrel_TigbayanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
